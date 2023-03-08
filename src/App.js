ed017a775efd942dc029ac21a22b5cffed721e22
vipin <vipinkumarlamba55@gmail.com>
2023-03-08 14:24:54 +0530

    counter app (usestate/useEffect) concepts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/header/Header';
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  // useEffect(() => {
+  //   alert("To changed the counter to " + count)
+  // },[count])
+
   return(
-    <main>Hello World</main>
+    <div className='container-fluid mt-5'>
+      <div className='row justify-content-center'>
+        <button className='btn btn-danger btn-sm' onClick={() => setCount((prevCount) => prevCount-1)}>-</button>
+        <h3>Count: {count}</h3>
+        <button className='btn btn-primary btn-sm' onClick={() => setCount((prevCount) => prevCount+1)}>+</button>
+      </div>
+    </div>
+    
   );
 }
 export default App;
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+
+  return (
+    <div className='container'>
+      
+    </div>
+  )
+}
+
+export default Header
